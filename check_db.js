4d1a1cd6e02f497814806a310569bd437ff47abf
Diyaa <kohlidiya02@gmail.com>
2026-04-07 03:44:52 +0530

    Remove non-essential documentation and assets from repository

--- check_db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Session = require('./backend/models/Session');
-require('dotenv').config({path: './backend/.env'});
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const sessions = await Session.find({ caseId: 'hello' });
-  console.log(JSON.stringify(sessions, null, 2));
-  process.exit();
-}).catch(console.error);
